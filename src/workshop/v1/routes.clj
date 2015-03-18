09809dfa5e0a0296c46710616cc01366deef43d2
Dave Moore <dave@8thlight.com>
2015-03-18 14:41:44 -0700

    Added OPTIONS as an option =)

--- src/workshop/v1/routes.clj
+++ src/workshop/v1/routes.clj
@@ -1,16 +1,23 @@
 (ns workshop.v1.routes
   (:require
-    [compojure.core :refer [defroutes context POST GET]]
+    [compojure.core :refer [defroutes context POST GET OPTIONS]]
     [workshop.controller :as controller]))
 
 
 (defroutes v1-handler
   (POST "/excavate" request (controller/excavate request))
   (POST "/excavate/" request (controller/excavate request))
+  (OPTIONS "/excavate" request (controller/excavate request))
+  (OPTIONS "/excavate/" request (controller/excavate request))
   (POST "/register" request (controller/register request))
   (POST "/register/" request (controller/register request))
-  (POST "/store" request (controller/store request))
+  (OPTIONS "/register" request (controller/register request))
+  (OPTIONS "/register/" request (controller/register request))
   (POST "/store" request (controller/store request))
   (POST "/store/" request (controller/store request))
+  (OPTIONS "/store" request (controller/store request))
+  (OPTIONS "/store/" request (controller/store request))
   (GET "/totals" request (controller/totals request))
-  (GET "/totals/" request (controller/totals request)))
+  (GET "/totals/" request (controller/totals request))
+  (OPTIONS "/totals" request (controller/totals request))
+  (OPTIONS "/totals/" request (controller/totals request)))
